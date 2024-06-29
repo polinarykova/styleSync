a5031c5543361673445c1faa4764131cc4fbf7f0
Polina <polina.rykova.hr@gmail.com>
2024-06-29 21:46:06 +0200

    #7 add initial structure to the welcome page

--- frontend/src/pages/Welcome.page.tsx
+++ frontend/src/pages/Welcome.page.tsx
@@ -1,3 +1,18 @@
 export default function () {
-  return <div>welcome</div>;
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="h-5/6 w-11/12 border-2 border-black">
+          <div className="flex flex-row h-full">
+            <div className="w-1/2 m-5 h-auto border-2 border-black hover:bg-blue-200">
+              a
+            </div>
+            <div className="w-1/2 m-5 h-auto border-2 border-black  hover:bg-blue-200">
+              b
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
