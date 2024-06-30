277b66223afc6dc485f42a0a86b2bffa88c14282
Polina <polina.rykova.hr@gmail.com>
2024-06-30 15:16:01 +0200

    #7 create layout for welcome page and connect it to the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
 import WelcomePage from "./pages/Welcome.page";
+import HomePage from "./pages/Home.page";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<WelcomePage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/Welcome.page.tsx
+++ frontend/src/pages/Welcome.page.tsx
@@ -4,11 +4,19 @@ export default function () {
       <div className="h-screen w-screen flex justify-center items-center">
         <div className="h-5/6 w-11/12 border-2 border-black">
           <div className="flex flex-row h-full">
-            <div className="w-1/2 m-5 h-auto border-2 border-black hover:bg-blue-200">
-              a
+            <div className="w-1/2 m-5 h-auto border-2 border-black hover:bg-blue-200 flex justify-center items-end">
+              <a href="/home">
+                <button className="border-blue-500 bg-blue-400 text-white m-10 p-5 rounded-md">
+                  Register
+                </button>
+              </a>
             </div>
-            <div className="w-1/2 m-5 h-auto border-2 border-black  hover:bg-blue-200">
-              b
+            <div className="w-1/2 m-5 h-auto border-2 border-black  hover:bg-blue-200 flex justify-center items-end">
+              <a href="/home">
+                <button className="border-blue-500 bg-blue-400 text-white m-10 p-5 rounded-md">
+                  Sign In
+                </button>
+              </a>
             </div>
           </div>
         </div>
